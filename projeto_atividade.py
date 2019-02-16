94eba8cd851479fd969aebd761ef1b1e99ee8f22
EDUARDO PATRICK PEREIRA CAVALCANTE <113019110@labin.canoas>
2019-02-15 21:18:39 -0300

    update 1.4

---
+++ projeto_atividade.py
@@ -0,0 +1,7 @@
+class Projeto_Atividade:
+    def __init__(self, projeto, atividade):
+        self.projeto = projeto
+        self.atividade = atividade
+
+def __str__(self):
+    return "Projeto: "+self.projeto.nome+" - Atividade: "+self.atividade.nome
